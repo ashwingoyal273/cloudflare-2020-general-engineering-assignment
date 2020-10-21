98bb1638c224765d182f7788d243df70aff77417
Ashwin Goyal <ashwingoyal@Ashwins-MacBook-Pro.local>
2020-10-20 19:04:12 -0700

    JSON \links api added

--- my-worker/index.js
+++ my-worker/index.js
@@ -1,12 +1,26 @@
+var links_arr = [{ "name": "Google", "url": "https://www.google.com" },
+{ "name": "Facebook", "url": "https://www.facebook.com" },
+{ "name": "Cloudflare", "url": "https://www.cloudflare.com" }
+]
+
 addEventListener('fetch', event => {
+  console.log(`Received new request: ${event.request.url}`)
   event.respondWith(handleRequest(event.request))
 })
+
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  if (request.url.endsWith("/links")) {
+  return new Response(JSON.stringify(links_arr), {
+    headers: { 'content-type': 'application/json' },
+  })}
+  else{
+    return new Response("Hello World", {
+      headers: { 'content-type' : 'text/plain' }
+    })
+  }
 }
